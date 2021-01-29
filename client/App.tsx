21aae4e3527f6d62e75a2394bb431b01342e0a46
Tngtater9 <jtater1@gmail.com>
2021-01-29 11:43:47 -0500

    fixed router

--- client/App.tsx
+++ client/App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import store from './store/store';
 import HomeScreen from './screens/home.screen';
+import RouterComponent from './router/router.component';
 
 export default function App() {
 	const Stack = createStackNavigator();
@@ -11,9 +12,7 @@ export default function App() {
 	return (
 		<Provider store={store}>
 			<NavigationContainer>
-				<Stack.Navigator>
-					<Stack.Screen name="Home" component={HomeScreen} />
-				</Stack.Navigator>
+				<RouterComponent></RouterComponent>
 			</NavigationContainer>
 		</Provider>
 	);
