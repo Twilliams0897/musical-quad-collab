68133cbe545ee78d0fee29d208316c9fb186fcaa
Yasmine Smith <yasmine.smith@revature.net>
2021-01-30 00:04:24 -0600

    Update login.component.tsx

--- client/user/login.component.tsx
+++ client/user/login.component.tsx
@@ -6,11 +6,7 @@ import {useDispatch, useSelector } from 'react-redux';
 import { getUser, loginAction } from '../store/actions';
 import { Button, TextInput, Text, View } from 'react-native';
 import style from '../global-styles';
-<<<<<<< Updated upstream
-import { User } from './user';
-=======
 import {User} from './user';
->>>>>>> Stashed changes
 
 // Function Component
 interface LoginProp {
@@ -19,7 +15,6 @@ interface LoginProp {
 function LoginComponent({navigation}: LoginProp) {
     const userSelector = (state: UserState) => state.loginUser;
     const user = useSelector(userSelector);
-    const actualUser = useSelector((state: UserState) => state.user);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -27,7 +22,6 @@ function LoginComponent({navigation}: LoginProp) {
         userService.getLogin().then((loggedUser)=>{
             dispatch(getUser(loggedUser));
             console.log(loggedUser.role, 'logged');
-            console.log(actualUser.username, 'actual user');
             navigation.navigate('Songs');
         }).catch((err)=>{
             console.error(err);
@@ -37,31 +31,12 @@ function LoginComponent({navigation}: LoginProp) {
     function submitForm() {
         userService.login(user).then((user) => {
             console.log(user);
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-            /*
-                When logged in, a new user with the same credentials is created. 
-                That way, when we click the back to the home page, the previous user is no longer logged in.
-            */
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> parent of 2d21ffd... No longer allows for previous user to remain logged in
             let newUser = new User();
             newUser.username = user.username;
             newUser.password = user.password;
             newUser.role = user.role;
             dispatch(getUser(newUser));
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-            navigation.navigate('Songs');  
-=======
             navigation.navigate('Songs');  //*
->>>>>>> Stashed changes
-=======
-            //dispatch(getUser(user));
-            navigation.navigate('Songs');  //*
->>>>>>> parent of 2d21ffd... No longer allows for previous user to remain logged in
         });      
          
     }
@@ -71,7 +46,7 @@ function LoginComponent({navigation}: LoginProp) {
             <Text>Username: </Text>
             <TextInput
                 style={style.input}
-                onChangeText={(value: any) =>
+                onChangeText={(value:any) =>
                     dispatch(loginAction({ ...user, username: value }))
                 }
                 value={user.username}
@@ -80,7 +55,7 @@ function LoginComponent({navigation}: LoginProp) {
             <TextInput
                 secureTextEntry={true}
                 style={style.input}
-                onChangeText={(value: any) =>
+                onChangeText={(value:any) =>
                     dispatch(loginAction({ ...user, password: value }))
                 }
                 value={user.password}
