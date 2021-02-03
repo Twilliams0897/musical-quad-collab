e226030ca1d51630a240fbb23137a4aabf6b6ea0
Jung Woo <michaelwoo921@gmail.com>
2021-02-03 11:19:33 -0800

    .

--- client/user/user.service.ts
+++ client/user/user.service.ts
@@ -5,8 +5,8 @@ class UserService {
 	private URI: string;
 	constructor() {
 		// URL of the express server
-		this.URI ='http://localhost:3000/users';
-		// this.URI = 'http://35.166.133.163:3000/users';
+		// this.URI ='http://localhost:3000/users';
+		this.URI = 'http://35.166.133.163:3000/users';
 	}
 	getLogin(): Promise<User> {
 		// withCredentials sends our cookies with the request.
