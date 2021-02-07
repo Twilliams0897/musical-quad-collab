0b688e5d283053e6053296ba2b403fb0dc5bd895
Yasmine Smith <yasmine.smith@revature.net>
2021-02-06 23:24:21 -0600

    troubleshooting add user and get users

--- client/user/user.service.ts
+++ client/user/user.service.ts
@@ -33,6 +33,7 @@ class UserService {
 	}
 
 	deleteByUsername(username: string): Promise<null> {
+		console.log(`user: ${username} was deleted.`)
 		return axios
 			.delete(this.URI + '/' + username, { withCredentials: true })
 			.then((result) => null)
@@ -47,15 +48,11 @@ class UserService {
 	
 	getUsers(): Promise<User []> {
 		return axios
-		.get(this.URI)
-		.then((result) => {
-			console.log(result.data);
-			return result.data;
-		}).catch((err) => {
+		.get(this.URI, {withCredentials: true})
+		.then(result => result.data).
+		catch((err) => {
 			console.error(err)
 		})
-			.then((result: any) => null)
-			.catch((err: any) => err);
 	}
 
 	//Look here to updat and add user... guesstimate what you need from the above
